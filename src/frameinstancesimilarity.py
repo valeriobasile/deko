3f94a72b26f98784876cb60fb7ece1b29cd995dc
valeriobasile <valeriobasile@gmail.com>
2017-07-20 11:14:23 +0200

    added frame element relatedness part of the equation

--- src/frameinstancesimilarity.py
+++ src/frameinstancesimilarity.py
@@ -2,9 +2,16 @@
 
 from random import random
 from nltk.corpus import framenet as fn
+from nltk.corpus import wordnet
 import math
+import os
+import logging as log
+
+# log configuration
+log.basicConfig(level=log.DEBUG, format='%(asctime)s.%(msecs)03d %(levelname)s %(message)s')
 
 # read the lexical units dictionary
+log.info("reading FrameNet lexical units")
 lexical_units = dict()
 with open("../resources/frame_lexical_units.tsv") as f:
     for line in f:
@@ -14,11 +21,12 @@ with open("../resources/frame_lexical_units.tsv") as f:
         lexical_units[frame].append(lu)
 
 # read Semcor lemmas:
+log.info("reading Semcor 3.0 lemmas")
 semcor_lemmas = dict()
 semcor_sentences = dict()
 with open("../resources/semcor3.0_lemmas.tsv") as f:
     for line in f:
-        sentence_id, lemma = line.rstrip().split('\t')
+        sentence_id, lemma, sense = line.rstrip().split('\t')
 
         if not sentence_id in semcor_lemmas:
             semcor_lemmas[sentence_id] = []
@@ -28,36 +36,125 @@ with open("../resources/semcor3.0_lemmas.tsv") as f:
             semcor_sentences[lemma] = []
         semcor_sentences[lemma].append(sentence_id)
 
+# Mapping different WN versions
+log.info("reading WordNet 3.0-3.1 mapping")
+wn30wn31 = dict()
+wn31wn30 = dict()
+with open(os.path.join(os.path.dirname(__file__), '../resources/wn30-31')) as f:
+    for line in f:
+        wn30, wn31 = line.rstrip().split(' ')
+        wn30wn31[wn30] = wn31
+        wn31wn30[wn31] = wn30
+
+# creating dictionary of Wordnet synset id (offsets)
+log.info("reading WordNet 3.0 offset-id mapping")
+offset2name = dict()
+name2offset = dict()
+with open("../resources/wordnet_offsets.tsv") as f:
+    for line in f:
+        offset, name = line.rstrip().split('\t')
+        offset2name[offset] = name
+        name2offset[name] = offset
+
 def frame_relatedness(frame1, frame2):
+    """Compute the relatedness between two frame types, using one of the
+    methods proposed by Pennacchiotti and Wirth (ACL 2009)."""
+    return cr_occ(frame1, frame2)
+
+def cr_occ(frame1, frame2):
+    """This is an implementation of the first co-occurrence measure of
+    Pennacchiotti and Wirth (ACL2009), described in Sec. 4.2.1 of the paper.
+    Input: two frame type names, e.g., Commerce_buy/Commerce_sell.
+    Output: a real number (Pointwise-mutual information)."""
+
     lus1 = lexical_units[frame1]
     lus2 = lexical_units[frame2]
 
+    '''Set of the contexts (sentences in Semcor 3.0) where the lexical units
+    of frame1 occur'''
     cf1 = set()
     for lu in lus1:
         if lu in semcor_sentences:
             cf1 = cf1.union(set(semcor_sentences[lu]))
 
+    '''Set the contexts (sentences in Semcor 3.0) where the lexical units
+    of frame2 occur'''
     cf2 = set()
     for lu in lus2:
         if lu in semcor_sentences:
             cf2 = cf2.union(set(semcor_sentences[lu]))
 
-    l1 = float(len(cf1))/36405.0
-    l2 = float(len(cf2))/36405.0
-    l12 = float(len(cf1.intersection(cf2)))/36405.0
+    '''Set of the contexts where lexical units from both frames co-occur'''
+    cf12 = cf1.intersection(cf2)
+
+    '''Compute the Point-wise Mutual Information'''
+    l1 = float(len(cf1))/float(len(semcor_lemmas))
+    l2 = float(len(cf2))/float(len(semcor_lemmas))
+    l12 = float(len(cf12))/float(len(semcor_lemmas))
     #print l1, l2, l12
     if l12 == 0.0 or l1 == 0.0 or l2 == 0.0:
         return 0.0
     else:
         return math.log(l12/(l1*l2), 2.0)
 
-def frame_element_relatedness(fe1, fe2):
-    return 0.0
+def wup_similarity(s1, s2):
+    synset1 = wordnet.synset(offset2name[s1])
+    synset2 = wordnet.synset(offset2name[s2])
+    return synset1.wup_similarity(synset2)
 
-def frame_instance_similarity(fi1, fi2):
+def synset_similarity(s1, s2):
+    """Input: two WordNet 3.0 synset identifier, e.g, '06326797-n'
+    Output: a real number between 0.0 and 1.0"""
+    return wup_similarity(s1, s2)
+
+def frame_element_relatedness(fe1, fe2, roles=False):
+    """Computes an aggregate measure of relatedness between the entities
+    involved in the frame elements.
+    Input: two frame elements
+    Output: a real number between 0.0 and 1.0"""
+    sims1 = []
+    for s1 in fe1:
+        max_sim = 0.0
+        for s2 in fe2:
+            if roles == True and s1.role != s2.role:
+                sim = 0.0
+            else:
+                synsetid1 = wn31wn30[s1.entity[1:-1].split('/')[-1]]
+                synsetid2 = wn31wn30[s2.entity[1:-1].split('/')[-1]]
+                sim = synset_similarity(synsetid1, synsetid2)
+            if sim > max_sim:
+                max_sim = sim
+        sims1.append(max_sim)
+
+    if len(sims1) > 0:
+        sim1 = sum(sims1)/float(len(sims1))
+    else:
+        sim1 = 0.0
+
+    sims2 = []
+    for s2 in fe2:
+        max_sim = 0.0
+        for s1 in fe1:
+            if roles == True and s1.role != s2.role:
+                sim = 0.0
+            else:
+                synsetid1 = wn31wn30[s1.entity[1:-1].split('/')[-1]]
+                synsetid2 = wn31wn30[s2.entity[1:-1].split('/')[-1]]
+                sim = synset_similarity(synsetid1, synsetid2)
+            if sim > max_sim:
+                max_sim = sim
+        sims2.append(max_sim)
+
+    if len(sims2) > 0:
+        sim2 = sum(sims2)/float(len(sims2))
+    else:
+        sim2 = 0.0
+
+    return (sim1+sim2)/2.0
+
+def frame_instance_similarity(fi1, fi2, alpha=0.5, roles=False):
     # the alpha prameter will be read from a config file
-    alpha = 1.0
     frame_sim = frame_relatedness(fi1.frame_type, fi2.frame_type)
-    fe_sim = frame_element_relatedness(fi1.frame_elements, fi2.frame_elements)
+    fe_sim = frame_element_relatedness(fi1.frame_elements, fi2.frame_elements, roles=roles)
     sim = alpha * frame_sim + (1.0-alpha) * fe_sim
     return sim
--- src/sentence_similarity.py
+++ src/sentence_similarity.py
@@ -3,37 +3,88 @@
 import sys
 import frameinstance
 import frameinstancesimilarity
+from optparse import OptionParser
+import logging as log
 
-document1 = frameinstance.read_instances_from_nt(sys.argv[1])
-document2 = frameinstance.read_instances_from_nt(sys.argv[2])
+# log configuration
+log.basicConfig(level=log.INFO, format='%(asctime)s.%(msecs)03d %(levelname)s %(message)s')
 
-sims = []
-for fid1, frameinstance1 in document1.iteritems():
-    max_sim = -1.0
-    for fid2, frameinstance2 in document2.iteritems():
-        sim = frameinstancesimilarity.frame_instance_similarity(frameinstance1, frameinstance2)
-        if sim > max_sim:
-            max_sim = sim
-    sims.append(max_sim)
+# command line argument partsing
+parser = OptionParser()
+parser.add_option('-i',
+                  '--input1',
+                  dest="input_file1",
+                  help="read triples of the first document from FILE1",
+                  metavar="FILE1")
+parser.add_option('-j',
+                  '--input2',
+                  dest="input_file2",
+                  help="read triples of the first document from FILE2",
+                  metavar="FILE2")
+parser.add_option('-l',
+                  '--file-list',
+                  dest="file_list",
+                  help="read pairs of documents from tab-separated file FILELIST",
+                  metavar="FILELIST")
+parser.add_option('-a',
+                  '--alpha',
+                  dest="alpha",
+                  help="alpha parameter for the frame instance similarity measure")
+parser.add_option('-r',
+                  '--roles',
+                  dest="roles",
+                  help="alpha parameter for the frame instance similarity measure")
 
-if len(sims)> 0:
-    avg_sim1 = sum(sims)/float(len(sims))
+(options, args) = parser.parse_args()
+
+if options.file_list:
+    with open(options.file_list) as f:
+        document_pairs = [line.rstrip().split('\t') for line in f.readlines()]
+elif options.input_file1 and options.input_file2:
+    document_pairs = [[options.input_file1, options.input_file2]]
 else:
-    avg_sim1 = 0.0
+    log.error("You must specify either a pair of input files or a file containing a list of triples file pairs.")
+    sys.exit(1)
 
-sims = []
-for fid2, frameinstance2 in document2.iteritems():
-    max_sim = -1.0
+if options.alpha:
+    alpha = eval(options.alpha)
+else:
+    alpha = 0.5
+
+roles = (options.roles != None and options.roles == 'true')
+
+for document_pair in document_pairs:
+    log.info('computing similarity between documents {0}, {1} (alpha={2}, roles={3})'.format(document_pair[0], document_pair[1], alpha, roles))
+    document1 = frameinstance.read_instances_from_nt(document_pair[0])
+    document2 = frameinstance.read_instances_from_nt(document_pair[1])
+
+    sims = []
     for fid1, frameinstance1 in document1.iteritems():
-        sim = frameinstancesimilarity.frame_instance_similarity(frameinstance1, frameinstance2)
-        if sim > max_sim:
-            max_sim = sim
-    sims.append(max_sim)
+        max_sim = -1.0
+        for fid2, frameinstance2 in document2.iteritems():
+            sim = frameinstancesimilarity.frame_instance_similarity(frameinstance1, frameinstance2, alpha=alpha, roles=roles)
+            if sim > max_sim:
+                max_sim = sim
+        sims.append(max_sim)
 
-if len(sims)> 0:
-    avg_sim2 = sum(sims)/float(len(sims))
-else:
-    avg_sim2 = 0.0
+    if len(sims)> 0:
+        avg_sim1 = sum(sims)/float(len(sims))
+    else:
+        avg_sim1 = 0.0
 
-avg_sim = (avg_sim1 + avg_sim2) / 2.0
-print avg_sim
+    sims = []
+    for fid2, frameinstance2 in document2.iteritems():
+        max_sim = -1.0
+        for fid1, frameinstance1 in document1.iteritems():
+            sim = frameinstancesimilarity.frame_instance_similarity(frameinstance1, frameinstance2, alpha=alpha, roles=roles)
+            if sim > max_sim:
+                max_sim = sim
+        sims.append(max_sim)
+
+    if len(sims)> 0:
+        avg_sim2 = sum(sims)/float(len(sims))
+    else:
+        avg_sim2 = 0.0
+
+    avg_sim = (avg_sim1 + avg_sim2) / 2.0
+    print avg_sim
